2ff06c638c0ce3cd1f416ab1ef0b5c58017acb59
Dillon Farber <dillon.farber@gmail.com>
2025-02-19 18:06:06 -0600

    project javascript updates

--- js/projects.js
+++ js/projects.js
@@ -1,40 +1,40 @@
-const map = {
-    0: {
+const map = new Map([
+    ['p0', {
         name: 'Nerd Haven App',
         lang: ['flutter', 'dart'],
-        description: '',
+        description: 'A one stop shop for all things media. From movies to games, games to books, and books to shows. Whatever you decide to put in your list, its yours. The Nerd Haven is a one stop shop for your backlog. Make ordered lists, ques, or just a junk drawer. Its up to you!',
         img: ['/media/imgs'],
         vid: ['/media/videos/listappprojectvideo.mp4'],
-    },
-    1: {
+    }],
+    ['p1', {
         name: 'Library Project program',
         lang: ['Java', 'SQL'],
         description: '',
         img: ['/media/imgs'],
         vid: ['/media/videos'],
-    },
-    2: {
+    }],
+    ['p2', {
         name: 'Machine Learning Project',
         lang: ['Python', 'Jypitur Notebooks'],
         description: '',
         img: ['/media/imgs'],
         vid: ['/media/videos'],
-    },
-    3: {
+    }],
+    ['p3', {
         name: 'Tomagatchie Project',
         lang: ['Python'],
         description: '',
         img: ['/media/imgs'],
         vid: ['/media/videos'],
-    },
-    4: {
+    }],
+    ['p4',{ 
         name: 'This Website',
         lang: ['HTML', 'CSS', 'JS'],
         description: '',
         img: ['/media/imgs'],
-        vid: ['/media/videos'],
-    }
-}
+        vid: ['/media/videos'],}
+    ]
+]);
 
 let id = 0;
 
@@ -46,7 +46,14 @@ window.onload = function () {
         tmp = params[i].split('=');
         data[tmp[0]] = tmp[1];
     }
-    id = document.getElementById('here').innerHTML = data.id;
-}
+    id = data.id;
+    document.getElementById('title').innerHTML = map.get(id).name;
+    document.getElementById('description').innerHTML = map.get(id).description;
+    for(let i = 0; i < map.get(id).vid.length; i++){
+        let media = document.getElementById('media');
+        document.getElementById('media').innerHTML += ('<div class=\"media-card\"><video class=\"video-card\" src=\"' + map.get(id).vid[i] +'\" controls autoplay ></video></div>');
+    }
+    for(let i = 0; i < map.get(id).img.length; i++){
 
-document.getElementById('title') = map[id].name;
+    }
+}
